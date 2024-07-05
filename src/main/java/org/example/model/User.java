6dbad9976a7f23040d2bb8c956244c5ce3dca80d
Zalesovsky <vladislav.zalesovski@gmail.com>
2024-07-06 01:22:27 +0300

    refactor: Added sl4j & lombok

--- src/main/java/org/example/model/User.java
+++ src/main/java/org/example/model/User.java
@@ -1,67 +1,19 @@
 package org.example.model;
 
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@EqualsAndHashCode
+@ToString
+@Builder
 public class User {
+
     private int id;
     private String firstName;
     private String secondName;
     private int age;
 
-    public User() {}
-
-    public User(String firstName, String secondName, int age) {
-        super();
-        this.firstName = firstName;
-        this.secondName = secondName;
-        this.age = age;
-    }
-
-    public User(int id, String firstName, String secondName, int age) {
-        super();
-        this.id = id;
-        this.firstName = firstName;
-        this.secondName = secondName;
-        this.age = age;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getSecondName() {
-        return secondName;
-    }
-
-    public void setSecondName(String secondName) {
-        this.secondName = secondName;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", firstName='" + firstName + '\'' +
-                ", secondName='" + secondName + '\'' +
-                ", age=" + age +
-                '}';
-    }
 }
