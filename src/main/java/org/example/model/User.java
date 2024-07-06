d2090d4b8e4d84de8a89ae9bb31c9f8d35d1a1eb
Arthur Auskern <chaplain04@gmail.com>
2024-07-06 11:22:15 +0300

    refactor: validation refactored with annotations

--- src/main/java/org/example/model/User.java
+++ src/main/java/org/example/model/User.java
@@ -1,9 +1,27 @@
 package org.example.model;
 
+import javax.validation.Constraint;
+import javax.validation.constraints.*;
+
 public class User {
+
     private int id;
+    @NotBlank(message = "Empty First Name field")
+    @NotNull(message = "Empty First Name field")
+    @NotEmpty(message = "Empty First Name field")
+    @Size(max = 50, message = "First Name field must be no more than 50 characters long")
+    @Pattern(regexp = "^[A-Za-z\\s]*$", message = "No non-latin characters allowed")
+    @Pattern(regexp = "^[^\\p{Punct}]*$", message = "No punctuation allowed")
     private String firstName;
+    @NotBlank(message = "Empty Second Name field")
+    @NotNull(message = "Empty Second Name field")
+    @NotEmpty(message = "Empty Second Name field")
+    @Size(max = 50, message = "Second Name field must be no more than 50 characters long")
+    @Pattern(regexp = "^[A-Za-z\\s]*$", message = "No non-latin characters allowed")
+    @Pattern(regexp = "^[^\\p{Punct}]*$", message = "No punctuation allowed")
     private String secondName;
+    @Min(value = 0, message = "Age is improbable")
+    @Max(value = 999, message = "Age is improbable")
     private int age;
 
     public User() {}
