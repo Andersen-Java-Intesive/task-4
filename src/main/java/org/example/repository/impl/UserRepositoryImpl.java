4f93770a88e882f3cd88da6c668da29740d46631
Issa <isa12345758@gmail.com>
2024-07-07 03:07:17 +0500

    additional commit

--- src/main/java/org/example/repository/impl/UserRepositoryImpl.java
+++ src/main/java/org/example/repository/impl/UserRepositoryImpl.java
@@ -6,30 +6,26 @@ import org.example.mapper.impl.UserMapperImpl;
 import org.example.model.User;
 import org.example.model.enums.Team;
 import org.example.repository.UserRepository;
-import org.example.service.DatabaseService;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
 
-import java.sql.*;
 import java.util.LinkedHashSet;
-
-import static java.sql.Connection.*;
+import java.util.List;
 
 public class UserRepositoryImpl implements UserRepository {
 
-    private static UserRepository instance;
-    private final DatabaseService databaseService = DatabaseService.getInstance();
+    private static UserRepositoryImpl instance;
     private final UserMapper userMapper = UserMapperImpl.getInstance();
-
-    private static final String INSERT_USERS_SQL = "INSERT INTO user_info (first_name, second_name, age, team) VALUES (?, ?, ?, ?)";
-    private static final String SELECT_USER_BY_ID = "SELECT id, first_name, second_name, age, team FROM user_info WHERE id = ?";
-    private static final String SELECT_ALL_USERS = "SELECT id, first_name, second_name, age, team FROM user_info ORDER BY id ASC";
-    private static final String SELECT_ALL_USERS_BY_TEAM = "SELECT id, first_name, second_name, age, team FROM user_info WHERE team = ? ORDER BY id ASC";
-    private static final String UPDATE_USERS_SQL = "UPDATE user_info SET first_name = ?, second_name = ?, age = ?, team = ? WHERE id = ?;";
-    private static final String DELETE_USERS_SQL = "DELETE FROM user_info WHERE id = ?;";
+    private final SessionFactory sessionFactory;
 
     private UserRepositoryImpl() {
+        sessionFactory = new Configuration().configure().buildSessionFactory();
     }
 
-    public static synchronized UserRepository getInstance() {
+    public static UserRepositoryImpl getInstance() {
         if (instance == null) {
             instance = new UserRepositoryImpl();
         }
@@ -38,113 +34,78 @@ public class UserRepositoryImpl implements UserRepository {
 
     @Override
     public boolean create(UserDto userDto) {
-        try (Connection connection = databaseService.getConnection(TRANSACTION_READ_UNCOMMITTED)) {
-            connection.setAutoCommit(false);
-            try (PreparedStatement preparedStatement = connection.prepareStatement(INSERT_USERS_SQL)) {
-                preparedStatement.setString(1, userDto.getFirstName());
-                preparedStatement.setString(2, userDto.getSecondName());
-                preparedStatement.setInt(3, userDto.getAge());
-                preparedStatement.setString(4, userDto.getTeam().toString());
-                preparedStatement.executeUpdate();
-            } catch (SQLException e) {
-                connection.rollback();
-                throw new RuntimeException(e);
+        Transaction transaction = null;
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+
+            User user = userMapper.mapUserDtoToUser(userDto);
+            session.save(user);
+
+            transaction.commit();
+            return true;
+        } catch (HibernateException e) {
+            if (transaction != null) {
+                transaction.rollback();
             }
-            connection.commit();
-        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-        return true;
     }
 
     @Override
     public User getById(int id) {
-        try (Connection connection = databaseService.getConnection(TRANSACTION_READ_COMMITTED)) {
-            try (PreparedStatement preparedStatement = connection.prepareStatement(SELECT_USER_BY_ID)) {
-                preparedStatement.setInt(1, id);
-                ResultSet resultSet = preparedStatement.executeQuery();
-                if (resultSet.next()) {
-                    return userMapper.mapResultSetToUser(resultSet);
-                } else {
-                    throw new RuntimeException("User with id " + id + " not found");
-                }
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
+        try (Session session = sessionFactory.openSession()) {
+            return session.get(User.class, id);
         }
     }
 
     @Override
     public LinkedHashSet<User> getAll() {
-        LinkedHashSet<User> users = new LinkedHashSet<>();
-        try (Connection connection = databaseService.getConnection(TRANSACTION_READ_COMMITTED)) {
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery(SELECT_ALL_USERS);
-            while (resultSet.next()) {
-                users.add(userMapper.mapResultSetToUser(resultSet));
-            }
-            return users;
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
+        try (Session session = sessionFactory.openSession()) {
+            List<User> users = session.createQuery("from User", User.class).list();
+            return new LinkedHashSet<>(users);
         }
     }
 
     @Override
     public LinkedHashSet<User> getAllByTeam(Team team) {
-        LinkedHashSet<User> users = new LinkedHashSet<>();
-        try (Connection connection = databaseService.getConnection(TRANSACTION_READ_COMMITTED)) {
-            try (PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ALL_USERS_BY_TEAM)) {
-                preparedStatement.setString(1, team.toString());
-                ResultSet resultSet = preparedStatement.executeQuery();
-                while (resultSet.next()) {
-                    users.add(userMapper.mapResultSetToUser(resultSet));
-                }
-                return users;
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
+        try (Session session = sessionFactory.openSession()) {
+            List<User> users = session.createQuery("from User where team = :team", User.class)
+                    .setParameter("team", team)
+                    .list();
+            return new LinkedHashSet<>(users);
         }
     }
 
     @Override
     public void update(UserDto userDto) {
-        try (Connection connection = databaseService.getConnection(TRANSACTION_REPEATABLE_READ)) {
-            connection.setAutoCommit(false);
-            try (PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_USERS_SQL)) {
-                if (getById(userDto.getId()) != null) {
-                    preparedStatement.setString(1, userDto.getFirstName());
-                    preparedStatement.setString(2, userDto.getSecondName());
-                    preparedStatement.setInt(3, userDto.getAge());
-                    preparedStatement.setString(4, userDto.getTeam().toString());
-                    preparedStatement.setInt(5, userDto.getId());
-                    preparedStatement.executeUpdate();
-                }
-            } catch (SQLException e) {
-                connection.rollback();
-                throw new RuntimeException(e);
+        Transaction transaction = null;
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            User user = userMapper.mapUserDtoToUser(userDto);
+            session.update(user);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
             }
-            connection.commit();
-        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-
     }
 
     @Override
     public void deleteById(int id) {
-        try (Connection connection = databaseService.getConnection(TRANSACTION_REPEATABLE_READ)) {
-            connection.setAutoCommit(false);
-            try (PreparedStatement preparedStatement = connection.prepareStatement(DELETE_USERS_SQL)) {
-                if (getById(id) != null) {
-                    preparedStatement.setInt(1, id);
-                    preparedStatement.executeUpdate();
-                }
-            } catch (SQLException e) {
-                connection.rollback();
-                throw new RuntimeException(e);
+        Transaction transaction = null;
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            User user = session.get(User.class, id);
+            if (user != null) {
+                session.delete(user);
             }
-            connection.commit();
-        } catch (SQLException e) {
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
             throw new RuntimeException(e);
         }
     }
--- src/main/java/org/example/service/DatabaseService.java
+++ src/main/java/org/example/service/DatabaseService.java
@@ -1,88 +1,25 @@
 package org.example.service;
 
-import java.io.InputStream;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.Properties;
-
-import static java.sql.Connection.*;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 public class DatabaseService {
 
-    private static DatabaseService instance;
+    private static SessionFactory sessionFactory;
 
-    private String databaseUrl;
-    private String databaseUsername;
-    private String databasePassword;
-
-    private DatabaseService() {
-        loadProperties();
-        setDatabaseDriver();
+    static {
         try {
-            Connection testConn = DriverManager.getConnection(databaseUrl, databaseUsername, databasePassword);
-            testConn.close();
-        } catch (SQLException e) {
-            throw new RuntimeException("Failed to establish database connection during initialization", e);
+            sessionFactory = new Configuration().configure().buildSessionFactory();
+        } catch (Throwable ex) {
+            throw new ExceptionInInitializerError(ex);
         }
     }
 
-    public Connection getConnection(int isolationLevel) {
-        Connection connection;
-        setDatabaseDriver();
-        try {
-            connection = DriverManager.getConnection(databaseUrl, databaseUsername, databasePassword);
-            connection.setTransactionIsolation(setDatabaseUser(isolationLevel));
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        return connection;
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
     }
 
-    private void loadProperties() {
-        try (InputStream input = getClass().getClassLoader().getResourceAsStream("secrets.properties")) {
-            if (input == null) {
-                throw new RuntimeException("Unable to find secrets.properties");
-            }
-            Properties prop = new Properties();
-            prop.load(input);
-            databaseUrl = prop.getProperty("DB_URL");
-            databaseUsername = prop.getProperty("DB_USERNAME");
-            databasePassword = prop.getProperty("DB_PASSWORD");
-        } catch (Exception e) {
-            throw new RuntimeException("Failed to load database properties", e);
-        }
+    public static void shutdown() {
+        getSessionFactory().close();
     }
-
-    public static synchronized DatabaseService getInstance() {
-        if (instance == null) {
-            instance = new DatabaseService();
-        }
-        return instance;
-    }
-
-    private void setDatabaseDriver() {
-        try {
-            Class.forName("org.postgresql.Driver");
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private static int setDatabaseUser(int isolationLevel) {
-        if (isolationLevel == TRANSACTION_READ_UNCOMMITTED) {
-            return TRANSACTION_READ_UNCOMMITTED;
-        }
-        if (isolationLevel == TRANSACTION_READ_COMMITTED) {
-            return TRANSACTION_READ_COMMITTED;
-        }
-        if (isolationLevel == TRANSACTION_REPEATABLE_READ) {
-            return TRANSACTION_REPEATABLE_READ;
-        }
-        if (isolationLevel == TRANSACTION_SERIALIZABLE) {
-            return TRANSACTION_SERIALIZABLE;
-        }
-        return TRANSACTION_READ_COMMITTED;
-    }
-
 }
