fe6feb50506a228bd94cddbea2f633a5d65b698e
Arthur Auskern <chaplain04@gmail.com>
2024-07-11 11:36:54 +0300

    fix: enum quickfix2

--- src/main/java/org/example/model/enums/Team.java
+++ src/main/java/org/example/model/enums/Team.java
@@ -1,30 +1,23 @@
 package org.example.model.enums;
 
 public enum Team {
-    NO_TEAM("NO_TEAM", "noteam"),
-    ORANGE_TEAM("ORANGE_TEAM", "orange"),
-    PINK_TEAM("PINK_TEAM", "pink");
+    NO_TEAM("noteam"),
+    ORANGE_TEAM( "orange"),
+    PINK_TEAM( "pink");
 
     private final String teamName;
 
-    public String getAlterTeamName() {
-        return alterTeamName;
-    }
-
-    private final String alterTeamName;
-
-    Team(String teamName, String alterTeamName) {
+    Team(String teamName) {
         this.teamName = teamName;
-        this.alterTeamName = alterTeamName;
     }
 
     public String getTeamName() {
         return teamName;
     }
 
-    public static Team fromString(String teamName) {
+    public static Team value(String teamName) {
         for (Team team : Team.values()) {
-            if (team.teamName.equalsIgnoreCase(teamName) || team.name().equalsIgnoreCase(teamName) || team.alterTeamName.equalsIgnoreCase(teamName)) {
+            if (team.teamName.equalsIgnoreCase(teamName) || team.name().equalsIgnoreCase(teamName)) {
                 return team;
             }
         }
